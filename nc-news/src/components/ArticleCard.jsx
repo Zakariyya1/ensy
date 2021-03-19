62f923388a80a435d9eab301685906e941d7c90c
Zakariyya <zakp100@gmail.com>
2021-03-19 16:11:38 +0000

    style article cards

--- nc-news/src/components/ArticleCard.jsx
+++ nc-news/src/components/ArticleCard.jsx
@@ -1,21 +1,30 @@
 import '../styles/ArticleCard.css';
 import React from 'react';
 import { Link } from '@reach/router';
+import Card from 'react-bootstrap/Card';
 
 const ArticleCard = (props) => {
   return (
-    <section className="articlecard">
-      <h3 className="title  ">
-        <Link to={`/articles/${props.article_id}`}>{props.title}</Link>{' '}
-      </h3>
-      <p className="by">
-        by <Link to={`/users/${props.author}/articles`}>{props.author}</Link>{' '}
-      </p>
-      <p className="topic">topic: {`${props.topic}`}</p>
-      <p className="commentnumber">
-        {`${props.comment_count}`} 💬 {`${props.votes}`} 👍{' '}
-      </p>
-    </section>
+    <Card
+      style={{
+        width: '18rem',
+        borderRadius: '0.5em'
+      }}
+      className="articlecard"
+    >
+      <Card.Body>
+        <h2 className="title">
+          <Link to={`/articles/${props.article_id}`}>{props.title}</Link>{' '}
+        </h2>
+        <Card.Text className="by">
+          by <Link to={`/users/${props.author}/articles`}>{props.author}</Link>{' '}
+        </Card.Text>
+        <Card.Text className="topic">topic: {`${props.topic}`}</Card.Text>
+        <Card.Text className="commentnumber">
+          {`${props.comment_count}`} 💬 {`${props.votes}`} 👍{' '}
+        </Card.Text>
+      </Card.Body>
+    </Card>
   );
 };
 
